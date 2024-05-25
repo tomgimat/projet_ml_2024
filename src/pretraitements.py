45547685dbe641c2460bf474a7fd14f0e70f2d4c
Tom Gimat <tom6gimat@gmail.com>
2024-05-25 16:35:32 +0200

    random forest

--- src/pretraitements.py
+++ src/pretraitements.py
@@ -18,11 +18,18 @@ from nltk.tokenize import word_tokenize
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 
+def nettoyer_texte(text):
+    text = clean_text(text)
+    tokens = tokenize(text)
+    tokens = [word for word in tokens if word not in stopwords.words("english")]
+    tokens = lemmatize_tokens(tokens)
+    return " ".join(tokens)
+
+
 #Suppression de certains caracteres
 def clean_text(text):
-    text = re.sub(r'<.*?>', '', text)
-    text = re.sub(r'http\S+', '', text)
-    text = re.sub(r'[^a-zA-Z\s]', '', text)
+    text = re.sub(r"\d+", "", text)
+    text = re.sub(r"\W+", " ", text)
     text = text.lower()
     return text
 
@@ -32,31 +39,11 @@ def tokenize(text):
     return word_tokenize(text)
 
 
-def remove_stopwords(tokens):
-    stop_words = set(stopwords.words('english'))
-    return [token for token in tokens if token not in stop_words]
-
-
 # LEMMATISATION
 from nltk.stem import WordNetLemmatizer
 nltk.download('wordnet')
 
+
 def lemmatize_tokens(tokens):
     lemmatizer = WordNetLemmatizer()
     return [lemmatizer.lemmatize(token) for token in tokens]
-
-
-#VECTORISATION TF-IDF 
-def vectorize_text_tfidf(texts):
-    vectorizer = TfidfVectorizer()
-    return vectorizer.fit_transform(texts)
-
-
-#Appel des fonctions sur l'ensemble des textes
-def complete_preprocess(texts):
-    clean_texts = [clean_text(text) for text in texts]
-    tokenized_texts = [tokenize(text) for text in clean_texts]
-    filtered_texts = [remove_stopwords(tokens) for tokens in tokenized_texts]
-    lemmatized_texts = [" ".join(lemmatize_tokens(tokens)) for tokens in filtered_texts]
-    vectorized_texts = vectorize_text_tfidf(lemmatized_texts)
-    return vectorized_texts
